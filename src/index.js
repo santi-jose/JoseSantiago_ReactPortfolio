f25a92b725de7ac1b0fac7817d4f3f1171cb76e3
jlsantia21 <jlsantia21@gmail.com>
2025-05-02 00:26:29 -0700

    Edited index.html to define title of portoflio. Also added a favicon and a assets folder in the public folder to add the favicon to. Edited the favicon link in the head element of index.html to add favicon png desired for portfolio.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-document.title = "Santi's Portfolio" // set title of website
 
 root.render(
   <React.StrictMode>
